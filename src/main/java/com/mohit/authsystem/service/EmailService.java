be6e0bd06953097a70fff017f10b318ce46c8c39
Mohit Malik <malikmohit332@gmail.com>
2026-07-26 10:56:16 +0530

    Added EmailService to send OTP via JavaMailSender

---
+++ src/main/java/com/mohit/authsystem/service/EmailService.java
@@ -0,0 +1,24 @@
+package com.mohit.authsystem.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendOtpEmail(String toEmail, String otp){
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setTo(toEmail);
+        message.setSubject("OTP Verification");
+        message.setText("Your Otp for Verification is : " +otp);
+
+        mailSender.send(message);
+    }
+}
