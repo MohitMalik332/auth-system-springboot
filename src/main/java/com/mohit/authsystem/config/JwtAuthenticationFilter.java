a7540acaaa19b81361da375accae82ad898a2c86
Mohit Malik <malikmohit332@gmail.com>
2026-07-29 13:31:00 +0530

    JWT authentication fully implemented

--- src/main/java/com/mohit/authsystem/config/JwtAuthenticationFilter.java
+++ src/main/java/com/mohit/authsystem/config/JwtAuthenticationFilter.java
@@ -1,17 +1,21 @@
 package com.mohit.authsystem.config;
 
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.User;
 import com.mohit.authsystem.util.JwtUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
 @RequiredArgsConstructor
@@ -38,9 +42,26 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         try {
             String email = jwtUtil.extractEmail(token);
 
-            // improve this later
-            if (email != null){
-                SecurityContextHolder.getContext().setAuthentication(null);
+
+            if (email != null && SecurityContextHolder.getContext().getAuthentication() == null){
+                // Spring Security User not the Entity
+                User userDetails = new User(
+                        email,
+                        "",
+                        Collections.singletonList(new SimpleGrantedAuthority("USER"))
+                );
+
+                // Creating Authentication Token
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails,
+                                null,
+                                userDetails.getAuthorities()
+                        );
+
+
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         } catch (Exception e) {
             System.out.println("Invalid JWT");
--- src/main/java/com/mohit/authsystem/config/SecurityConfig.java
+++ src/main/java/com/mohit/authsystem/config/SecurityConfig.java
@@ -1,13 +1,19 @@
 package com.mohit.authsystem.config;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -21,7 +27,14 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                //Stateless JWT
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                // JWT Filter
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
